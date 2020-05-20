85404f2751ee98ed5c55bfbf01796ceff6fb9968
ojasbhamare <ojasbhamare@users.noreply.github.com>
2020-05-20 13:08:57 +0000

    automated commit by check50 [check50=True]

--- runoff.c
+++ runoff.c
@@ -130,9 +130,10 @@ bool vote(int voter, int rank, string name)
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        if(strcmp(candidates[i].name, name) == 0)
+        if (strcmp(candidates[i].name, name) == 0)
         {
             preferences[voter][rank] = i;
+            return true;
         }
     }
     return false;
