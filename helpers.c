c515250259d26835817ace3e1c9df96e45e1d7a9
ojasbhamare <ojasbhamare@users.noreply.github.com>
2020-05-24 00:15:31 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -110,7 +110,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
             int sumr = 0;
             int sumg = 0;
             int sumb = 0;
-            int count = 0;
+            float count = 0;
             for (int h = i - 1; h <= h+1; h++)
             {
                 for(int k = j - 1; k <= k+1; k++)
@@ -124,9 +124,9 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                     }
                 }
             }
-            int ansr = round(sumr / (float)count);
-            int ansg = round(sumg / (float)count);
-            int ansb = round(sumb / (float)count);
+            int ansr = round(sumr / count);
+            int ansg = round(sumg / count);
+            int ansb = round(sumb / count);
 
             image[i][j].rgbtRed = ansr;
             image[i][j].rgbtGreen = ansg;
